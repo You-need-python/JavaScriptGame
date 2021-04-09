3e1b5a8bcc77d12494d423039639235fad7ea92c
You-need-python <taehyuk0402@naver.com>
2021-04-09 10:46:02 +0000

    djdsj

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,19 @@
 $(function(){
-  var trigger = $('#trigger');
-  var trigger2 = document.getElementById('trigger')
-  var menu = $('.header ul');
+  const trigger = $('#trigger');
+  const menu = $('.header ul');
   $(trigger).on('click',function(e){
     e.preventDefault();
     menu.slideToggle();
   });
+})
+
+$(function(){
+  const typing = [$('.card-typing'), 'TypingGame/index.html'];
+  
+  openGame(typing[0],typing[1]);
+  function openGame(game, link){
+    $(game).on('click',function(){
+      window.open(link);
+    })
+  }
 })
